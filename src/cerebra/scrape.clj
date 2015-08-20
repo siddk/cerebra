a61ea0f5961fdfb60245032a052f6347e5c83df6
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2015-08-20 16:22:50 -0700

    Start Python

--- src/cerebra/scrape.clj
+++
@@ -1,6 +0,0 @@
-;;;; scrape.clj
-
-;;;; Pull in article data from the Marvel wiki, and store the html data (with hyperlinks) as
-;;;; separate documents.
-
-(ns cerebra.scrape)
--- test/cerebra/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns cerebra.core-test
-  (:require [clojure.test :refer :all]
-            [cerebra.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
